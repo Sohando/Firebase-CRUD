7d880e732d0b7e03c4e66e50156faa90cea163fb
Sohando <sohandoabernahin1@gmail.com>
2020-09-18 01:19:29 +0600

    Done keya's blockchain work upto search and mutation

--- BlockChain_Keya/js/main.js
+++ BlockChain_Keya/js/main.js
@@ -1,7 +1,8 @@
-var found;
+var found; 
+var match;
 var prev_name;
 var obj;
-
+var update_key;
 var firebaseConfig = {
     apiKey: "AIzaSyD-yD16zThYc4uGEqomMA-ryAMctH2jxew",
     authDomain: "blockchain-d9757.firebaseapp.com",
@@ -19,6 +20,7 @@ if (!firebase.apps.length) {
 
 function Registration() {
     const rootRef = firebase.database().ref("resources/");
+    var autoID = rootRef.push().key;
     rootRef.push({
         plot_no: localStorage.getItem("plot_no"),
         owner_name: localStorage.getItem("own_name"),
@@ -29,7 +31,10 @@ function Registration() {
         prev_owner_name: localStorage.getItem("prev_own_name"),
         prev_owner_nid: localStorage.getItem("prev_own_nid"),
         prev_owner_email: localStorage.getItem("prev_own_email"),
-        reg_date: localStorage.getItem("reg_date")
+        reg_date: localStorage.getItem("reg_date"),
+        autoid: autoID,
+        mutation_no: "",
+        mutation_date: ""
     });
     localStorage.setItem("Registration", 0);
 }
@@ -37,12 +42,16 @@ function Registration() {
 function Search(key) {
     found = false;
     const rootRef = firebase.database().ref("resources/");
+    listTableHead = document.getElementById("head");
+    listTableBody = document.getElementById("body");
+    listTableBody.textContent = "";
     rootRef.on("value",
         (snapshot) => {
             snapshot.forEach((child) => {
                 resouces = child.val();
-                console.log(resouces.plot_no);
+                // console.log(resouces.plot_no);
                 if (resouces.plot_no == key) {
+                    // console.log(resouces.prev_owner_name + " prev");
                     notify = document.getElementById("x");
                     notify.textContent = ""
                     obj = {
@@ -55,18 +64,15 @@ function Search(key) {
                     found = true;
                     //return;
                     // document.getElementById("tbl").style.display="visible";
-                    listTableHead = document.getElementById("head");
                     listTableHead.textContent = "";
                     var row = document.createElement("tr");
                     row.innerHTML =
-                        "<th>" + "Previous Owner's Name" + "</th>" +
-                        "<th>" + "Previous Owner's NID" + "</th>" +
-                        "<th>" + "New Owner's Name" + "</th>" +
-                        "<th>" + "New Owner's NID" + "</th>" +
-                        "<th>" + "Registration Date" + "</th>";
+                         "<th>" + "Previous Owner's Name" + "</th>" +
+                         "<th>" + "Previous Owner's NID" + "</th>" +
+                         "<th>" + "New Owner's Name" + "</th>" +
+                         "<th>" + "New Owner's NID" + "</th>" +
+                         "<th>" + "Registration Date" + "</th>";
                     listTableHead.append(row);
-                    listTableBody = document.getElementById("body");
-                    listTableBody.textContent = "";
                     var row = document.createElement("tr");
                     row.innerHTML =
                         "<td>" + obj.PName + "</td>" +
@@ -92,6 +98,46 @@ function Search(key) {
         notify.textContent = "Not Found";
         // alert("hidden");
     }
+}
+function Match(plot, nid) {
+    const rootRef = firebase.database().ref("resources/");
+    // alert(plot + " " + nid)
+    // console.log(rootRef);
+    match = false;
+    rootRef.on("value",
+    (snapshot) => {
+        snapshot.forEach((child) => {
+            issue = child.val();
+             console.log(issue.plot_no);
+            // alert("asdfasd");
+                // alert(issue.plot_no + " " + issue.owner_nid); 
+                if (issue.plot_no == plot && issue.owner_nid==nid) {
+                    // match = true;
+                    // alert(this.value + " " + child.key);
+                    // issue.autoid;
+                    // update_key = child.key;
+                    localStorage.setItem("u_key", child.key);
+                    // alert(update_key);
+                    window.location.href = "mutation2.html";
+                }
+         });
+    },
+    (error) => {
+        console.log("Error: " + error.code);
+    }
+    );
+    match = false;
+}
+function Update(mutation_no, mutation_date) {
+    // alert(mutation_no + " " + mutation_date);
+    update_key = localStorage.getItem("u_key");
+    alert(update_key);
+    var recordRef = firebase.database().ref("resources/" + update_key);
+        recordRef.update({
+            "mutation_no" : mutation_no,
+            "mutation_date" : mutation_date
+    });
+    document.getElementById("X").innerHTML = "Successfully added" + mutation_no;
 }
 if (localStorage.getItem("Registration") != 0) {
     Registration();
